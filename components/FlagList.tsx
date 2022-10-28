0d86d27db590a0e9a5b9d98d32b8f3b618be134b
Moe_azaza <mohamed@ranadev.io>
2022-10-28 17:12:51 +0100

    fix

---
+++ components/FlagList.tsx
@@ -0,0 +1,66 @@
+import { Text, View, ScrollView, Image } from "react-native";
+import i18n from "../i18n/tanslations";
+import { flagListStyles as styles } from "../styles";
+import { flagIcon, bugIcon } from "../assets";
+import { list } from "../data/dummy";
+
+export default function FlagList() {
+  return (
+    <ScrollView style={styles.container}>
+      {list.map((item, index) => (
+        <View key={index} style={styles.flagContainer}>
+          <View style={styles.header}>
+            <Image source={flagIcon} style={styles.img} />
+            <Text style={styles.date}>{item.date}</Text>
+          </View>
+          <View style={styles.grid}>
+            <View style={styles.col}>
+              <View style={styles.title}>
+                <Text style={styles.titleTxt}>{i18n.t("riskCategory")}</Text>
+              </View>
+              <View style={styles.selection}>
+                <Image style={styles.image} source={bugIcon} />
+              </View>
+              <View style={styles.name}>
+                <Text style={styles.nameTxt}>{item.risk}</Text>
+              </View>
+            </View>
+            <View style={styles.col}>
+              <View style={styles.title}>
+                <Text style={styles.titleTxt}>{i18n.t("pestType")}</Text>
+              </View>
+              <View style={styles.selection}>
+                <Image style={styles.image} source={bugIcon} />
+              </View>
+              <View style={styles.name}>
+                <Text style={styles.nameTxt}>{item.type}</Text>
+              </View>
+            </View>
+            <View style={styles.col}>
+              <View style={styles.title}>
+                <Text style={styles.titleTxt}>{i18n.t("plantPart")}</Text>
+              </View>
+              <View style={styles.selection}>
+                <Image style={styles.image} source={bugIcon} />
+              </View>
+              <View style={styles.name}>
+                <Text style={styles.nameTxt}>{item.part}</Text>
+              </View>
+            </View>
+            <View style={styles.col}>
+              <View style={styles.title}>
+                <Text style={styles.titleTxt}>{i18n.t("location")}</Text>
+              </View>
+              <View style={styles.selection}>
+                <Image style={styles.image} source={bugIcon} />
+              </View>
+              <View style={styles.name}>
+                <Text style={styles.nameTxt}>{item.location}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
